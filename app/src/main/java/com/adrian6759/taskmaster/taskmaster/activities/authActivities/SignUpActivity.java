141cd82ecfcdba92badfca5dba6f79823f3b32b8
adrian.hernandez1993@yahoo.com <adrianhernandez@Adrians-MacBook-Pro-2.local>
2023-02-28 02:41:57 -0800

    Added authorization to application

---
+++ app/src/main/java/com/adrian6759/taskmaster/taskmaster/activities/authActivities/SignUpActivity.java
@@ -0,0 +1,50 @@
+package com.adrian6759.taskmaster.taskmaster.activities.authActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.adrian6759.taskmaster.R;
+import com.amplifyframework.auth.AuthUserAttributeKey;
+import com.amplifyframework.auth.options.AuthSignUpOptions;
+import com.amplifyframework.core.Amplify;
+
+public class SignUpActivity extends AppCompatActivity {
+    public static final String TAG = "sign_up_activity";
+    public static final String USER_EMAIL = "user_email";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        setupSignUpButton();
+    }
+
+    public void setupSignUpButton(){
+        findViewById(R.id.SignUpButton).setOnClickListener(view -> {
+
+       String userEmail = ((EditText)findViewById(R.id.editTextTextEmailAddress)).getText().toString();
+        String userPassword = ((EditText)findViewById(R.id.editTextTextPassword)).getText().toString();
+
+            Amplify.Auth.signUp(
+                userEmail,
+                userPassword,
+                AuthSignUpOptions.builder()
+                        .userAttribute(AuthUserAttributeKey.email(), userEmail)
+                        .userAttribute(AuthUserAttributeKey.nickname(), "Oso")
+                        .build(),
+                success -> Log.i(TAG, "Signed Up Successfully!"),
+                failure -> Log.e(TAG, "Failed to signup with email: " + userEmail + failure)
+
+        );
+            Intent goToConfirmSignUpActivity = new Intent(this, VerifySignUp.class);
+            goToConfirmSignUpActivity.putExtra(USER_EMAIL, userEmail);
+            startActivity(goToConfirmSignUpActivity);
+        });
+
+    }
+}
---
+++ app/src/main/java/com/adrian6759/taskmaster/taskmaster/activities/authActivities/VerifySignUp.java
@@ -0,0 +1,56 @@
+package com.adrian6759.taskmaster.taskmaster.activities.authActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.adrian6759.taskmaster.R;
+import com.adrian6759.taskmaster.taskmaster.adapter.TasksRecyclerViewAdapter;
+import com.amplifyframework.core.Amplify;
+
+public class VerifySignUp extends AppCompatActivity {
+    public static final String TAG = "verify_signup";
+    Intent callingIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_sign_up);
+        callingIntent = getIntent();
+
+        Intent callingIntent = getIntent();
+        String taskTitle = null;
+        String taskBody = null;
+        String taskState = null;
+        if (callingIntent != null) {
+            taskTitle = callingIntent.getStringExtra(TasksRecyclerViewAdapter.TASKS_TITLE_TAG);
+            taskBody = callingIntent.getStringExtra(TasksRecyclerViewAdapter.TASKS_BODY_TAG);
+            taskState = callingIntent.getStringExtra(TasksRecyclerViewAdapter.TASKS_STATE_TAG);
+        }
+    }
+
+    public void setupConfirmationButton(){
+        findViewById(R.id.SubmitConfirmationButton).setOnClickListener(view ->{
+
+            String userEmail = "";
+            if (callingIntent != null){
+                userEmail = callingIntent.getStringExtra(SignUpActivity.USER_EMAIL);
+            }
+            String confirmationCode = ((EditText) findViewById(R.id.editTextConfirmationCode)).getText().toString();
+
+            Amplify.Auth.confirmSignUp(
+                userEmail,
+                confirmationCode,
+                success -> Log.i(TAG, "Confirmed Successfully!"),
+                failure -> Log.e(TAG, "FAILED to verify confirmation code" + confirmationCode + "for user: with failure" + failure)
+                );
+            Intent goToLoginActivityIntent = new Intent(this, LoginActivity.class);
+            goToLoginActivityIntent.putExtra(SignUpActivity.USER_EMAIL, userEmail);
+            startActivity(goToLoginActivityIntent);
+                });
+    }
+}
